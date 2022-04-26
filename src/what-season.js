5486dc2e8f3b1dc714dc15436654191c5d5fb270
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-04-26 13:10:46 +0300

    add some solutions1

--- src/what-season.js
+++ src/what-season.js
@@ -12,8 +12,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-    throw new NotImplementedError('Not implemented');
-    if (!date instanceof Date && !isNaN(date)) { new Error('Invalid date!') }
+    // throw new NotImplementedError('Not implemented');
+    if (!date instanceof Date && !isNaN(date)) { throw new Error('Invalid date!') }
     if (date === undefined) { return 'Unable to determine the time of year!' }
 
     const seasons = ["spring", "summer", "autumn", "winter"];
@@ -21,24 +21,24 @@ function getSeason(date) {
     const day = +date.getDate()
     const year = date.getFullYear()
 
-    if (!m || !day || !year) { new Error('Invalid date!') }
+    if (!m || !day || !year) { throw new Error('Invalid date!') }
 
-    if (year < 1970) { new Error('Invalid date!') }
-    if (date - new Date(0) < 0) { new Error('Invalid date!') }
-    if (day < 1 || day > 31) { new Error('Invalid date!') }
-    if (day === 31 && m !== 0 || m !== 2 || m !== 4 || m !== 6 || m !== 7 || m !== 9 || m !== 11) { new Error('Invalid date!') }
-    if (day === 30 && m !== 3 || m !== 5 || m !== 8 || m !== 10) { new Error('Invalid date!') }
-    if (day === 29 && m !== 1) { new Error('Invalid date!') }
-    if (day === 28 && m !== 1) { new Error('Invalid date!') }
-
-
-
-    if (m === 1 && day === 28 && (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) { new Error('Invalid date!') }
-    if (m === 1 && day === 29 && !(year % 4 === 0 && year % 100 !== 0) || !year % 400 === 0) { new Error('Invalid date!') }
+    if (year < 1970) { throw new Error('Invalid date!') }
+    if (date - new Date(0) < 0) { throw new Error('Invalid date!') }
+    if (day < 1 || day > 31) { throw new Error('Invalid date!') }
+    if (day === 31 && m !== 0 || m !== 2 || m !== 4 || m !== 6 || m !== 7 || m !== 9 || m !== 11) { throw new Error('Invalid date!') }
+    if (day === 30 && m !== 3 || m !== 5 || m !== 8 || m !== 10) { throw new Error('Invalid date!') }
+    if (day === 29 && m !== 1) { throw new Error('Invalid date!') }
+    if (day === 28 && m !== 1) { throw new Error('Invalid date!') }
 
 
 
-    if (m < 0 || m > 11) return 'Invalid date!'
+    if (m === 1 && day === 28 && (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) { throw new Error('Invalid date!') }
+    if (m === 1 && day === 29 && !(year % 4 === 0 && year % 100 !== 0) || !year % 400 === 0) { throw new Error('Invalid date!') }
+
+
+
+    if (m < 0 || m > 11) { throw new Error('Invalid date!') }
     if (m >= 0 && m < 2 || m === 11) return seasons[3]
     if (m > 1 && m < 5) return seasons[0]
     if (m > 4 && m < 8) return seasons[1]
