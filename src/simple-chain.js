e9a174e58dfaffd3f85789910476ea541b904e2a
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-01 09:28:13 +0300

    feat: add solution vigenere

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -18,7 +18,9 @@ const chainMaker = {
     },
     removeLink(position) {
 
-        if (typeof position !== 'number' || position < 1 || position >= this.chain.split('~~').length - 1) { throw new Error('You can\'t remove incorrect link!') }
+        if (typeof position !== 'number') throw new Error('You can\'t remove incorrect link!')
+        if (position < 1) throw new Error('You can\'t remove incorrect link!')
+        if (position >= this.chain.split('~~').length - 1) throw new Error('You can\'t remove incorrect link!')
 
         this.chain = this.chain.split('~~').slice(0, position - 1).concat(this.chain.split('~~').slice(position, this.chain.split('~~').length - 1)).join('~~') + '~~'
         return this;
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,50 +20,91 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-    constructor(key) {
-        this.key = key;
+    constructor(bool = true) {
+        this.bool = bool;
+    }
+    isUpperCase(item) {
+        let c = item.charCodeAt();
+        if (c >= 65 && c <= 90) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    // (this.bool===false)? directMachine:reverseMachine
-    // directMachine() {}
-    // reverseMachine() {}
+    isLowerCase(item) {
+        let c = item.charCodeAt();
+        if (c >= 97 && c <= 122) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    encrypt(str, key) {
+        if (str == undefined) throw new Error('Incorrect arguments!')
+        if (key == undefined) throw new Error('Incorrect arguments!')
+        if (arguments.length === 0) throw new Error('Incorrect arguments!')
 
-    encrypt(text) {
-        let cipher = '';
-        text = text.toUpperCase();
-        for (let i = 0, k = 0; i < text.length; i++) {
-            const tCode = text[i].charCodeAt(0);
-            // console.log(tCode);
-            if (tCode >= 65 && tCode <= 90) {
-                const charCode = ((tCode) + (this.key.charCodeAt(k % this.key.length))) % 26;
-                cipher += String.fromCharCode(charCode + 65);
-                k++
+        let up, low, l, f,
+            crypt = "",
+            k = 0
+        for (let i = 0; i < str.length; i++) {
+            l = str[i];
+            let lCode = l.charCodeAt()
+            let kCode = key[k % key.length]
+
+            if (this.isUpperCase(l)) {
+                f = 65
+                kCode = kCode.toUpperCase().charCodeAt()
+                up = (lCode + kCode - 2 * f) % 26;
+                crypt += String.fromCharCode(up + f);
+                k++;
+            } else if (this.isLowerCase(l)) {
+                f = 97
+                kCode = kCode.toLowerCase().charCodeAt()
+                low = (lCode + kCode - 2 * f) % 26;
+                crypt += String.fromCharCode(low + f);
+                k++;
             } else {
-                cipher += text[i];
+                crypt += l;
             }
         }
-        return cipher;
+        return this.bool ? crypt.toUpperCase() : crypt.split('').reverse().join('').toUpperCase();
     };
 
-    decrypt(cipher) {
-        let text = '';
-        for (let i = 0, k = 0; i < cipher.length; i++) {
-            const tCode = cipher[i].charCodeAt(0);
-            // console.log(tCode);
-            // console.log(cipher.charCodeAt(i));
+    decrypt(crypt, key) {
+        if (crypt == undefined) throw new Error('Incorrect arguments!')
+        if (key == undefined) throw new Error('Incorrect arguments!')
+        if (arguments.length === 0) throw new Error('Incorrect arguments!')
 
-            if (tCode >= 65 && tCode <= 90) {
-                const charCode = ((((tCode) - (this.key.charCodeAt(k % this.key.length))) % 26 + 26) % 26);
-                text += String.fromCharCode(charCode + 65);
-                k++
+        let up, low, l, f,
+            str = "",
+            k = 0
+        for (let i = 0; i < crypt.length; i++) {
+            l = crypt[i];
+            let lCode = l.charCodeAt()
+            let kCode = key[k % key.length]
+            if (this.isUpperCase(l)) {
+                f = 65
+                kCode = kCode.toUpperCase().charCodeAt()
+                up = (((lCode - kCode) % 26 + 26) % 26);
+                str += String.fromCharCode(up + f);
+                k++;
+            } else if (this.isLowerCase(l)) {
+
+                f = 97
+                kCode = kCode.toLowerCase().charCodeAt()
+                low = (((lCode - kCode) % 26 + 26) % 26);
+                str += String.fromCharCode(low + f);
+                k++;
             } else {
-                text += cipher[i];
+                str += l;
             }
-
         }
-        return text;
+        return this.bool ? str.toUpperCase() : str.split('').reverse().join('').toUpperCase();
     }
 }
+
 module.exports = {
     VigenereCipheringMachine
 };
